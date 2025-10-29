f149f37401510cb331918b2ad94caa3f7c0abe3c
Sahil Padole <sahilpadole28@gmail.com>
2025-10-30 00:24:09 +0530

    Added new Files and new Features

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 // added new feature--button
 // added new feature --form
+let thnx=document.getElementById("thnx");
+let btn=document.getElementById("one");
+btn.addEventListener("click",()=>{
+    alert(`The button was clicked \n${new Date}`);
+    setTimeout(() => {
+        thnx.innerHTML="Thank You !!";
+    }, 1000);
+    setTimeout(() => {
+        thnx.innerHTML="";
+    }, 6000);
+});
