5ecfd42bab549af87af35c2a69fd59f5c9c104d9
Sahil Padole <sahilpadole28@gmail.com>
2025-10-30 00:35:18 +0530

    Added a new Maths Feature.

---
+++ math.js
@@ -0,0 +1,9 @@
+function add(a,b){
+    return a+b;
+}
+function multiply(a,b){
+    return a*b;
+}   
+let x=parseInt(prompt("Enter a:"));
+let y=parseInt(prompt("Enter b:"));
+console.log(`Sum of ${a} and ${b} is ${add(x,y)}`);
